afecc67450ad5302ee4b9a62a20d457ad55d8caa
Florian <merklefl@icloud.com>
2020-11-04 11:06:43 +0000

    add evaluation method

--- src/helpers.py
+++ src/helpers.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
-def train(model, train_data, test_data, epochs):
+def train(model, train_data, test_data, epochs, device):
     for epoch in range(epochs):  # loop over the dataset multiple times
         criterion = nn.CrossEntropyLoss()
         optimizer = optim.Adam(model.parameters())
@@ -12,7 +12,7 @@ def train(model, train_data, test_data, epochs):
             # get the inputs; data is a list of [inputs, labels]
             #if i == 0:
             inputs, labels = data
-                
+            inputs, labels = inputs.to(device), labels.to(device)
 
             # zero the parameter gradients
             optimizer.zero_grad()
@@ -30,18 +30,19 @@ def train(model, train_data, test_data, epochs):
             print('[%d, %5d] loss: %.5f' %(epoch + 1, i + 1, running_loss))
             print('[%d, %5d] test_accuracy: %.2f' %(epoch + 1, i + 1, accuracy))
             running_loss = 0.0
-        accuracy = evaluate_model(model, test_data)
+        accuracy = evaluate_model(model, test_data, device)
         print('val_accuracy: ', accuracy)
         
     print('Finished Training')
     return True
-def evaluate_model(model, data_loader):
+def evaluate_model(model, data_loader, device):
     correct = 0
     total = 0
     with torch.no_grad():
         for i, data in enumerate(data_loader):
             print(i)
             images, labels = data
+            images, labels = images.to(device), labels.to(device)
             outputs = model(images)
             #print(outputs)
             _, predicted = torch.max(outputs.data, 1)
